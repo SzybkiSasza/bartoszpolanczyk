63735a66f3077d8ba1787ff2459f6e65f9284af0
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-05-18 02:15:53 -0700

    Modified front end files a bit.

--- src/website/index.tsx
+++ src/website/index.tsx
@@ -1,1 +1,11 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import { Hello } from 'website/components/hello';
+
 import './index.scss';
+
+ReactDOM.render(
+  <Hello compiler="TypeScript" framework="React" />,
+  document.getElementById('hello'),
+);
