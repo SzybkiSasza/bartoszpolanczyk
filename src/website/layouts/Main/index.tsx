f94607f6891e128d2df71f6c7d84dbffdb3fe546
Bartek Polanczyk <bartosz.polanczyk@gmail.com>
2018-05-23 03:03:53 -0700

    Added fancy animation.

--- src/website/layouts/Main/index.tsx
+++ src/website/layouts/Main/index.tsx
@@ -7,7 +7,9 @@ import './Main.css';
 export const Main = () => (
   <div className="main">
     <Header/>
-    <div className="main__part main__part--left"></div>
-    <div className="main__part main__part--right"></div>
+    <div className="main__sliding-container">
+      <div className="main__part main__part--left"></div>
+      <div className="main__part main__part--right"></div>
+    </div>
   </div>
 );
